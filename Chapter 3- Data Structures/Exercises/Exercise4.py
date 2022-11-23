b2008d01a5cef5da5739589a7792ee35e3ee4106
Ravinder-Harshitha <115135659+Ravinder-Harshitha@users.noreply.github.com>
2022-11-23 10:17:24 -0800

    Create Exercise4.py
    Chapter 3, Exercise 4

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+guests = ["APJ Abdul Kalam","Swami Vivekananda","Nicola Tesla"]
+
+invite = ("Greetings! " + guests[0]+" , please join us for dinner.")
+print(invite)
+invite = ("Greetings! " + guests[1]+" , please join us for dinner.")
+print(invite)
+invite = ("Greetings! " + guests[2]+" , please join us for dinner.")
+print(invite)
