4b43a7b5acd710526b3b00708338d70345017a67
Ravinder-Harshitha <115135659+Ravinder-Harshitha@users.noreply.github.com>
2022-11-23 10:19:17 -0800

    Create Exercise7.py
    Chapter 3, Exercise 7

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,30 @@
+places = ["Australia","France","Italy","Japan","Iceland"]
+print("Original Order:")
+print(places)
+
+print("\nIn Alphabetical Order:")
+print(sorted(places))
+
+print("\nOriginal Order:")
+print(places)
+
+print("\nIn Reverse Alphabetical Order:")
+print(sorted(places,reverse=True))
+
+print("\nOriginal Order:")
+print(places)
+
+print("\nIn Reverse Order:")
+places.reverse()
+print(places)
+
+print("\nOriginal Order:")
+print(places)
+
+print("\nIn Alphabetical Order:")
+places.sort()
+print(places)
+
+print("\nIn Reverse Alphabetical Order:")
+places.sort(reverse=True)
+print(places)
