e6602bd3e53be47aa96a5cb8934f621732eaaba8
Ravinder-Harshitha <115135659+Ravinder-Harshitha@users.noreply.github.com>
2022-11-24 14:53:10 -0800

    Create Exercise3.py
    Chapter 7, Exercise 3

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+
+def make_shirt(size, message):
+  print("\n A " + size + " size t-shirt is to be made that will say, " + message)
+
+make_shirt("medium", '"Coding is Fun!"') 
+make_shirt('large', '"Python Rocks"') 
+
