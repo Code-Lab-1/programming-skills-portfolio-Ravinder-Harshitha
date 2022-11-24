c1f8a564dd25fe40f284a1ccb0f911ba1435615d
Ravinder-Harshitha <115135659+Ravinder-Harshitha@users.noreply.github.com>
2022-11-24 07:51:13 -0800

    Create Exercise1.py
    Chapter 4, Exercise 1

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,11 @@
+
+alien_color="green"
+if alien_color=="green":
+  print("Comgratulations! You just earned 5 points.")
+
+alien_color="yellow"
+if alien_color=="green":
+  print("Congatulations! You just earned 5 points.")
+
+
+  
